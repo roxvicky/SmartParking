9a790941b5f24b1b89de52d87fb53be8e6ff0fdc
Kittin T. <userkittin@hotmail.com>
2014-09-04 16:10:09 +0700

    prepare environment for FindMyCar

--- SmartParking/Findcar.xaml.cs
+++ SmartParking/Findcar.xaml.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Device.Location;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Microsoft.Phone.Maps.Controls;
+using Windows.Devices.Geolocation;
 
 namespace SmartParking
+
 {
     public partial class Findcar : PhoneApplicationPage
     {
@@ -16,5 +20,20 @@ namespace SmartParking
         {
             InitializeComponent();
         }
+
+        public static GeoCoordinate ConvertGeocoordinate(Geocoordinate geocoordinate)
+        {
+            return new GeoCoordinate
+                (
+                geocoordinate.Latitude,
+                geocoordinate.Longitude,
+                geocoordinate.Altitude ?? Double.NaN,
+                geocoordinate.Accuracy,
+                geocoordinate.AltitudeAccuracy ?? Double.NaN,
+                geocoordinate.Speed ?? Double.NaN,
+                geocoordinate.Heading ?? Double.NaN
+                );
+        }
+
     }
 }
