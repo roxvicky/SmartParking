9c4ffae57bee582c99707908ed6468ebe6dc756a
Kittin T. <userkittin@hotmail.com>
2014-11-04 20:46:03 +0700

    กฟหกฟหกฟหก

--- SmartParking/Checkin.xaml.cs
+++ SmartParking/Checkin.xaml.cs
@@ -40,6 +40,7 @@ namespace SmartParking
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         DbHelper DB_helper = new DbHelper();
         History His = new History();
         
@@ -58,12 +59,16 @@ namespace SmartParking
 =======
         History store = new History();
 >>>>>>> parent of 3494e9d... update
+=======
+        History store = new History();
+>>>>>>> parent of 3494e9d... update
         
 
         public Checkin()
         {
             InitializeProximityDevice();
             InitializeComponent();
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -80,6 +85,8 @@ namespace SmartParking
 >>>>>>> parent of 3494e9d... update
 =======
 >>>>>>> 09f72465158335e3abaed6a5b570ed90672dbcd3
+=======
+>>>>>>> parent of 3494e9d... update
 =======
 >>>>>>> parent of 3494e9d... update
 =======
@@ -121,6 +128,7 @@ namespace SmartParking
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
      
 
 =======
@@ -129,6 +137,8 @@ namespace SmartParking
 >>>>>>> parent of 3494e9d... update
 =======
 >>>>>>> 09f72465158335e3abaed6a5b570ed90672dbcd3
+=======
+>>>>>>> parent of 3494e9d... update
 =======
 >>>>>>> parent of 3494e9d... update
 =======
@@ -165,6 +175,7 @@ namespace SmartParking
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                     SetFloorStatus("Latitude: " + latitude + "   Longitude: " + longtitude);
                    // DB_helper.AddInfo();
                     //His.ReadHistoryList_Loaded();
@@ -185,6 +196,11 @@ namespace SmartParking
                     store.AddDb(Floor_st, Zone_st, Latitud_do, Longtitude_do);
 
 >>>>>>> 09f72465158335e3abaed6a5b570ed90672dbcd3
+=======
+                    SetFloorStatus("Longitude: " + latitude + "   Longitude: " + longtitude);
+                    store.AddDb(Floor_st, Zone_st, Latitud_do, Longtitude_do);
+
+>>>>>>> parent of 3494e9d... update
 =======
                     SetFloorStatus("Longitude: " + latitude + "   Longitude: " + longtitude);
                     store.AddDb(Floor_st, Zone_st, Latitud_do, Longtitude_do);
